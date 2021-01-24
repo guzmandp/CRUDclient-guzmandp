df0aecf04b52f54df0b97a16764f03fd67e37ec7
Darwin Guzman <guzmandp@gmail.com>
2021-01-24 19:26:58 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Darwin', '70812811259', 3000.0, TIMESTAMP WITH TIME ZONE '1983-06-20T13:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Luisa', '70812805259', 7000.0, TIMESTAMP WITH TIME ZONE '1978-10-20T11:25:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Luisa', '70812805259', 7000.0, TIMESTAMP WITH TIME ZONE '1978-10-20T11:25:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria', '557988773', 6500.0, TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 1);
