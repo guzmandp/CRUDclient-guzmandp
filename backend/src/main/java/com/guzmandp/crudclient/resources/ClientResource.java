3c89794ec37dcb7da05100cc577973983b155893
Darwin Guzman <guzmandp@gmail.com>
2021-01-25 19:38:03 -0300

    Paginacao

--- backend/src/main/java/com/guzmandp/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/guzmandp/crudclient/resources/ClientResource.java
@@ -1,9 +1,11 @@
 package com.guzmandp.crudclient.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,8 +29,17 @@ public class ClientResource {
 	private ClientService service;
 	
 		@GetMapping
-		public ResponseEntity<List<ClientDTO>> findAll(){
-			List<ClientDTO> list = service.findAll();
+		public ResponseEntity<Page<ClientDTO>> findAll(
+				@RequestParam(value = "page", defaultValue = "0") Integer page,
+				@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+				@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+				@RequestParam(value = "orderBy", defaultValue = "name") String orderBy)
+							
+				{
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy );
+			
+			Page<ClientDTO> list = service.findAllPaged(pageRequest);
+			
 			return ResponseEntity.ok().body(list);
 		}
 		@GetMapping(value = "/{id}")
@@ -39,6 +51,7 @@ public class ClientResource {
 		@PostMapping
 		public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
 			dto = service.insert(dto);
+			@SuppressWarnings("unused")
 			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 			.buildAndExpand(dto.getId()).toUri();	
 			return ResponseEntity.ok().body(dto);		
